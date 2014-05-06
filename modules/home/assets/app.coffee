79c7ffabf3e80987d7238ffb58a94cfbe5b2122f
Lukas Alexandre <lukas@codelogic.me>
2014-05-06 16:46:39 +0200

    Moving assets to views folder

--- modules/home/assets/app.coffee
+++
@@ -1,7 +0,0 @@
-# s = Snap "#logo-big"
-
-# polygon = s.polygon [150, 150, 100]
-# bigCircle.attr
-#   fill: "#bada55"
-#   stroke: "#000"
-#   strokeWidth: 5
--- modules/home/router.rb
+++ modules/home/router.rb
@@ -5,23 +5,8 @@ require 'stylus'
 require 'stylus/tilt'
 
 class Stik::Home < Sinatra::Base
-  get '/vendor/*.js' do |file|
-    send_file File.join(__dir__, "assets/vendor/#{file}.js")
-  end
-
-  get '/assets/stik.min.js' do
-    send_file File.join(__dir__, "assets/stik.min.js")
-  end
-
-  get '/assets/*.*' do |file, ext|
-    case ext
-    when 'css'
-      stylus File.read(File.join(__dir__, "assets/#{file}.styl"))
-    when 'js'
-      coffee File.read(File.join(__dir__, "assets/#{file}.coffee"))
-    else
-      fail 'file not found'
-    end
+  get '/assets/:file.css' do |file|
+    stylus "stylesheets/#{file.to_sym}".to_sym
   end
 
   get '/?' do
